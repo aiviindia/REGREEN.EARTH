7d8d00ec5994afd55f2d3ce1918ec8a8d9e2014e
gigachadcodder <singhpratapabhay60@gmail.com>
2024-09-27 16:48:39 +0530

    all new fixes

--- src/Pages/Home/Components/OfferingsCards.jsx
+++ src/Pages/Home/Components/OfferingsCards.jsx
@@ -6,14 +6,17 @@ const OfferingsCards = ({ logo, title, content }) => {
     <div className="flex flex-col gap-2 w-[20rem]  rounded-3xl border-white border-2 text-white px-6 py-4 hover:bg-green-600">
       <div className="flex items-center gap-5">
         <img
-          src={logo}
+          src={logo || newlogo}
           alt="Climate Tech Logo"
           className="block"
           style={{ width: "26.97px", height: "31px" }}
         />
-        <h1>{title}</h1>
+        <h1>{title || "services"}</h1>
       </div>
-      <div>{content}</div>
+      <div>
+        {content ||
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, placeat?"}
+      </div>
     </div>
   );
 };
